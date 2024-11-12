e770d6cbc284655b48fc086017256305167a2943
SuprajaQA <psupraja11ss@gmail.com>
2024-11-13 00:00:10 +0530

    12

---
+++ Nov/ex_09112024_Literals_Variables/Lab029_task12112024.py
@@ -0,0 +1,14 @@
+#Task
+#take 3 inputs from user
+#perform sum, sub, mul and div
+
+a=int(input("enter a"))
+b=int(input("enter b"))
+c=int(input("enter c"))
+
+'''print(a+b+c)
+print(a-b-c)
+print(a*b*c)
+print(a/b/c)'''
+
+print((a+b+c),"sum",'\n',(a-b-c),"sub",'\n',(a*b*c),"mul",'\n',(a/b/c),"div")
---
+++ Nov/ex_09112024_Literals_Variables/Lab030_task212112024.py
@@ -0,0 +1,9 @@
+#Task 2
+#take 2 inputs from user
+#print the quotient and reminder
+
+a=int(input("enter num1"))
+b=int(input("enter num2"))
+q=a//b
+r=a%b
+print(q,r)
