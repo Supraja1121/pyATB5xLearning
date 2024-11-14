71c0b6d533fab77fb13ffd3bee96ed852fb9c509
SuprajaQA <psupraja11ss@gmail.com>
2024-11-14 10:35:07 +0530

    12

--- Nov/ex_12112024_Operators/Lab044_Task1_ifelse.py
+++ Nov/ex_12112024_Operators/Lab044_Task1_ifelse.py
@@ -1,8 +1,27 @@
 #write a program to let the user know if he/she can attend the club
 #age is 21
 
+#logic building
+
+# Step 1
+# use input i/p -> data type -> int
+#o/p > string > user can go
+
+# Step 2 Rough logic
+# age >21 -> can go
+# age <21 -> cant go
+
+# Step 3
+
 age =int(input("enter your age:\n"))
-if age>=21:
+if age>=21 and age<=130:
     print("you can go to the Club")
 else:
-    print("You are not eligible to go to the Club")
+    print(f"You are not eligible to go to the Club:{age}")
+
+# Step 4 Check for edge cases
+#negative input
+#non numeric value
+#long
+
+# Step 5 Optimize the code
--- Nov/ex_12112024_Operators/Lab046_Task3.py
+++ Nov/ex_12112024_Operators/Lab046_Task3.py
@@ -1,4 +1,22 @@
-#Problem to find the max between two ( 3,4) → 4
-data=3,4
-a=max(3,4)
-print(a)
+#Problem to find the max between three numbers
+
+#logic building
+# user inputs int
+#output int
+
+#logic : if there is 1 condition we use "if else"
+# If there are more conditions we use if elif else
+
+
+a=int(input("num1\n"))
+b=int(input("num2\n"))
+c=int(input("num3\n"))
+
+if a>b and a>c:
+    print("max", a)
+elif b>a and b>c:
+    print("max", b)
+else:print("max",c)
+
+d=max(a,b,c)
+print("max is",d)
